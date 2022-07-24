77aded0cf61e3a6b89c51b635a60df25e4a50582
Richard Zins <rizins@zins.dynu.com>
2022-07-23 20:59:10 -0700

    Added TLS support. Right now its self signed but when deployed I will use a CA...

--- auth-page-backend/src/main.rs
+++ auth-page-backend/src/main.rs
@@ -3,14 +3,14 @@ use rand::seq::SliceRandom;
 
 const MNEMONICS: &'static [&'static str] = &["Please Excuse My Dear Aunt Sally", "Eggs Are Deliciously Good Breakfast Energy", "Fat Alley Cats Eat Alot Of Garbage", "All Cows Eat Lots Of Green Grass", "Goblins Bring Death For All Creatures"];
 
-fn getMnemonic() -> String {
+fn get_mnemonic() -> String {
     MNEMONICS
     .choose(&mut rand::thread_rng())
     .unwrap()
     .to_string()
 }
 
-fn getPasswordFromMnemonic(mnemonic: &String) -> String {
+fn get_password_from_mnemonic(mnemonic: &String) -> String {
     let initials: String = mnemonic
     .split(" ")                     // create an iterator, yielding words
     .flat_map(|s| s.chars().nth(0)) // get the first char of each word
@@ -25,9 +25,9 @@ fn index() -> &'static str {
 
 #[get("/mnemonic")]
 fn mnemonic() -> String {
-    let randMnemonic = getMnemonic();
-    let pass = getPasswordFromMnemonic(&randMnemonic);
-    format!("Mnemonic: {}. So your password is {}!", randMnemonic, pass)
+    let rand_mnemonic = get_mnemonic();
+    let pass = get_password_from_mnemonic(&rand_mnemonic);
+    format!("Mnemonic: {}. So your password is {}!", rand_mnemonic, pass)
 }
 
 #[launch]
